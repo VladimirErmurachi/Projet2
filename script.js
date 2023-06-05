53c2ad653f20c4828f7e420e57e8289f169bfa6f
Vladimir <ermurachi.vladimir@mail.ru>
2023-06-05 19:54:15 +0300

    random

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ p2.addEventListener('mouseover', ()=>{
 p3.addEventListener('dblclick', ()=>{
     p2.style.background = 'green'
 })
+
+window.addEventListener('keydown',(ev)=>{
+    if(ev.keyCode === 82){
+        p1.style.background = getRandomColor()
+        p2.style.background = getRandomColor()
+        p3.style.background = getRandomColor()
+      
+    }
+    else if (ev.keyCode === 49 ){
+        p1.style.background = getRandomColor();
+    }
+    else if( ev.keyCode === 50){
+        p2.style.background = getRandomColor()
+    }
+    else if(ev.keyCode === 51){
+        p3.style.background = getRandomColor()
+    }
+    
+    
+})
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+getRandomColor(); 
